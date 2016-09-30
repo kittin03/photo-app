a9cfa5f524b82ef70667db0cfc77b0358e4c094f
kittin03 <katarinam@centrum.cz>
2016-09-30 02:08:16 +0200

    cleanup

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Photo from './Photo';
 import Comments from './Comments';
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import css from './index.css';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import './index.css';
+import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 import App from './components/App';
